5ee23293063d364ec430a4f03b5c32be62c3895b
liujie <572890972@qq.com>
2023-06-05 14:17:40 +0800

    Add cool face api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const cool = require('cool-ascii-faces')
 
 const PORT = process.env.PORT || 5001
 
@@ -8,4 +9,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
